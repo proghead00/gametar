bc3a393e76b2beb558469384ac64d0d50ad21ffd
proghead00 <susnatadj.rocks@gmail.com>
2021-01-05 18:53:37 +0530

    screenshot url added

--- src/api.js
+++ src/api.js
@@ -1,4 +1,3 @@
-//Base URL
 const base_url = "https://api.rawg.io/api/";
 
 //Getting the date
@@ -28,7 +27,6 @@ const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
 const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
 const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 
-//Popular Games
 const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
 const upcoming_games = `games?dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
 const newGames = `games?dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
@@ -36,3 +34,8 @@ const newGames = `games?dates=${lastYear},${currentDate}&ordering=-released&page
 export const popularGamesURL = () => `${base_url}${popular_games}`;
 export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
 export const newGamesURL = () => `${base_url}${newGames}`;
+
+export const gameDetailsURL = (game_id) => `${base_url}games/${game_id}`;
+
+export const gameScreenshotsURL = (game_id) =>
+	`${base_url}games/${game_id}/screenshots`;
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,10 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { useDispatch } from "react-redux";
+import { loadDetail } from "../actions/detailAction";
 
-const Game = ({ name, released, image }) => {
+const Game = ({ name, released, image, id }) => {
+	const dispatch = useDispatch();
+	const loadDetailHandler = () => {
+		dispatch(loadDetail(id));
+	};
 	return (
-		<StyledGame>
+		<StyledGame onClick={loadDetailHandler}>
 			<h3>{name}</h3>
 			<p>{released}</p>
 			<img src={image} alt={name} />
